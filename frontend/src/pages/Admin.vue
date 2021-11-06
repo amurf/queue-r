599ef717c1bd33bf0840fe7e948684abe0631c9e
ash <ash@strategicdata.com.au>
2021-11-06 21:28:29 +1100

    Refactor admin page to not show QR code

--- frontend/src/pages/Admin.vue
+++ frontend/src/pages/Admin.vue
@@ -1,21 +1,78 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { reactive } from "vue";
 import axios from "axios";
 
-import QRCode from "qrcode";
+const order = reactive({
+  items: ["Flat white", "Peanut butter brownie"],
+  name: "",
+  qrCode: "",
+  submitted: false,
+});
 
-const fullName: String = ref("");
-const qrCode: String = ref("");
+function reset() {
+  order.name = "";
+  order.qrCode = "";
+}
 
-async function fetchQrCode() {
-  let { data } = await axios.post("/submit/job", { fullName: fullName.value });
-  qrCode.value = data;
+async function submit() {
+  let { data } = await axios.post("/submit/job", {
+    name: order.name,
+  });
+
+  window.open(`/qr/${data.id}?name=${order.name}`);
+  reset();
 }
 </script>
 
 <template>
-  <input type="text" v-model="fullName" />
-  <button @click="fetchQrCode()">Gogo</button>
-  <div v-html="qrCode"></div>
+  <div class="w-full h-screen flex flex-col justify-center items-center px-5">
+    <template v-if="order.submitted">
+      <div v-html="order.qrCode" class="qrcode-container"></div>
+      <p>
+        Hi {{ order.name }}, scan this QR code to keep track of your order,
+        we'll let you know when it's ready
+      </p>
+
+      <button @click="reset()" class="border p-2 bg-blue-200 rounded shadow">
+        Next order
+      </button>
+    </template>
+    <template v-else>
+      <div class="border shadow p-4 mb-2">
+        <h1 class="font-bold text-lg">Order</h1>
+        <ul>
+          <li v-for="item in order.items">{{ item }}</li>
+        </ul>
+      </div>
+
+      <form @submit.prevent="submit()" class="flex flex-col text-center">
+        <label for="name" class="font-bold text-lg">Customer name</label>
+        <input
+          type="text"
+          v-model="order.name"
+          id="name"
+          class="border shadow p-2"
+        />
+
+        <button
+          type="submit"
+          class="border p-2 mt-2 shadow rounded bg-blue-200"
+        >
+          Submit
+        </button>
+      </form>
+    </template>
+  </div>
 </template>
 
+<style scoped>
+.qrcode-container {
+  width: 30%;
+}
+
+@media only screen and (max-width: 600px) {
+  .qrcode-container {
+    width: 100%;
+  }
+}
+</style>
