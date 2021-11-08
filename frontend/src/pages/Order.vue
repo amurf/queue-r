b4ee08e0ac1e4040f78addfa82f31437fc5f51a0
ash <ash@strategicdata.com.au>
2021-11-08 14:39:48 +1100

    remove old QR code from order page
    This is now living in its own page + component

--- frontend/src/pages/Order.vue
+++ frontend/src/pages/Order.vue
@@ -23,42 +23,26 @@ async function submit() {
 
 <template>
   <div class="flex flex-col items-center justify-center w-full h-screen px-5">
-    <template v-if="order.submitted">
-      <div v-html="order.qrCode" class="qrcode-container"></div>
-      <p>
-        Hi {{ order.name }}, scan this QR code to keep track of your order,
-        we'll let you know when it's ready
-      </p>
+    <div class="p-4 mb-2 border shadow">
+      <h1 class="text-lg font-bold">Order</h1>
+      <ul>
+        <li v-for="item in order.items">{{ item }}</li>
+      </ul>
+    </div>
 
-      <button @click="reset()" class="p-2 bg-blue-200 border rounded shadow">
-        Next order
+    <form @submit.prevent="submit()" class="flex flex-col text-center">
+      <label for="name" class="text-lg font-bold">Customer name</label>
+      <input
+        type="text"
+        v-model="order.name"
+        id="name"
+        class="p-2 border shadow"
+      />
+
+      <button type="submit" class="border p-2 mt-2 shadow rounded bg-blue-200">
+        Submit
       </button>
-    </template>
-    <template v-else>
-      <div class="p-4 mb-2 border shadow">
-        <h1 class="text-lg font-bold">Order</h1>
-        <ul>
-          <li v-for="item in order.items">{{ item }}</li>
-        </ul>
-      </div>
-
-      <form @submit.prevent="submit()" class="flex flex-col text-center">
-        <label for="name" class="text-lg font-bold">Customer name</label>
-        <input
-          type="text"
-          v-model="order.name"
-          id="name"
-          class="p-2 border shadow"
-        />
-
-        <button
-          type="submit"
-          class="border p-2 mt-2 shadow rounded bg-blue-200"
-        >
-          Submit
-        </button>
-      </form>
-    </template>
+    </form>
   </div>
 </template>
 
