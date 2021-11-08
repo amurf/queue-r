fae721d3c4f91f18733c1812b1abe51dad786ae9
ash <ash@strategicdata.com.au>
2021-11-08 14:00:14 +1100

    Rename admin to order and move to / route

--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory, RouterView } from "vue-router";
 
 import Track from "./pages/Track.vue";
-import Admin from "./pages/Admin.vue";
+import Order from "./pages/Order.vue";
 import Manage from "./pages/Manage.vue";
 import QR from "./pages/QR.vue";
 
@@ -10,7 +10,7 @@ export const router = createRouter({
   history: routerHistory,
   strict: true,
   routes: [
-    { path: "/admin", name: "admin", component: Admin },
+    { path: "/", name: "order", component: Order },
     { path: "/manage", name: "manage", component: Manage },
     { path: "/qr/:id", name: "qr", component: QR, props: true },
     { path: "/w/:id", name: "track", component: Track, props: true },
