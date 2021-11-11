ecda9a3e5c5916511ace18bce3d26fe51918bdcf
ash <ash@strategicdata.com.au>
2021-11-12 09:00:53 +1100

    Add koa-logger to job-submit

--- job-submit/src/server.js
+++ job-submit/src/server.js
@@ -1,13 +1,15 @@
 const Koa = require("koa");
-const koaBody = require("koa-body");
-
 const app = new Koa();
 
+const body = require("koa-body");
 app.use(
-  koaBody({
+  body({
     jsonLimit: "1kb",
   })
 );
+
+const logger = require("koa-logger");
+app.use(logger());
 
 const router = require("./router.js");
 app.use(router.routes()).use(router.allowedMethods());
