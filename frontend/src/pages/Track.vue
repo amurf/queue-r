a10f7656312deab3248bc4316f09b3e8488e8c18
ash <ash@strategicdata.com.au>
2021-11-20 15:24:21 +1100

    Restyle tracking page

--- frontend/src/pages/Track.vue
+++ frontend/src/pages/Track.vue
@@ -2,6 +2,8 @@
 import { io } from "socket.io-client";
 import { ref } from "vue";
 
+import Logo from "../images/logo-dark.svg";
+
 const props = defineProps({
   id: {
     type: String,
@@ -23,31 +25,52 @@ socket.on("update", (newStatus) => {
 socket.on("error", (newStatus) => {
   error.value = newStatus;
 });
+
+document.body.style.setProperty("--colour-body", "var(--colour-primary-light)");
 </script>
 
 <template>
-  <div class="flex flex-col items-center justify-center w-full h-screen px-5">
+  <div class="flex flex-col items-center justify-between w-full h-screen p-4">
+    <Logo class="text-white" />
+
     <div
       class="
         flex flex-col
         items-center
-        w-1/2
+        w-4/5
         bg-purple-100
-        border
-        rounded
         shadow
+        border
+        p-4
+        rounded-md
+        text-3xl text-white
       "
     >
-      <h1 class="p-2 text-xl font-bold border border-bottom">
-        Your order is being prepared
-      </h1>
+      <h1 class="font-bold">Order status</h1>
 
-      <p class="w-full border-purple-200 border-t p-2 text-center">
+      <p class="">
         <template v-if="error">Something went wrong</template>
         <template v-else>
           {{ status }}
         </template>
       </p>
+    </div>
+
+    <p class="text-white text-xl font-bold w-3/4 text-2xl">
+      We'll call you when your order is ready, sit tight!
+    </p>
+
+    <div class="grid grid-cols-2 gap-2">
+      <button
+        class="text-white border border-primary-dark p-2 rounded-md text-xl"
+      >
+        Name wrong? Let us know.
+      </button>
+      <button
+        class="text-white border border-primary-dark p-2 rounded-md text-xl"
+      >
+        See QR code
+      </button>
     </div>
   </div>
 </template>
