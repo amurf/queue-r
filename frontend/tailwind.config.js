90cec7c304a1cdc6481690f81ff7c3789bd637d6
ash <ash@strategicdata.com.au>
2021-11-20 15:22:38 +1100

    Setup colours in tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,18 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      primary: {
+        dark: "var(--colour-primary-dark)",
+        light: "var(--colour-primary-light)",
+      },
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+    },
     extend: {},
   },
   variants: {
